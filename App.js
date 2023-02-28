13d7b4b5b4fcde340d0ae305625ba48b70c4f281
Amad Arshad <amadarshad@gmail.com>
2023-02-28 23:09:38 +0800

    add new files

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date }
+    console.debug("test message")
+  }
+
+  
+  componentDidMount() {
+
+    setInterval(() => { this.tick() }, 1000)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.Timer)
+    console.debug("test message2")
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> The Time is now:
+          {this.state.date.toLocaleTimeString()}
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
